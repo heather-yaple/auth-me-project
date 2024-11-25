1a1681e98183fc7ac37c463767d3ebd896a83173
heather-yaple <heatherlyaple@gmail.com>
2024-11-25 18:47:29 -0500

    Delete Confirmation Modal

--- frontend/src/components/DeleteReviewConfirmationModal/DeleteReviewConfirmationModal.jsx
+++ frontend/src/components/DeleteReviewConfirmationModal/DeleteReviewConfirmationModal.jsx
@@ -1,7 +1,6 @@
 // src/components/DeleteReviewConfirmationModal/DeleteReviewConfirmationModal.jsx
 
-// eslint-disable-next-line no-unused-vars
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { deleteReview } from '../../store/reviews';
 import './DeleteReviewConfirmationModal.css';
@@ -15,32 +14,39 @@ const DeleteReviewConfirmationModal = ({ reviewId, closeModal }) => {
       await dispatch(deleteReview(reviewId));
       closeModal(); // Close the modal after deletion
     } catch (err) {
-      const errorMessages = err.errors ? Object.values(err.errors) : [];
+      const errorMessages = err.errors ? Object.values(err.errors) : ["An unexpected error occurred. Please try again later."];
       setErrors(errorMessages);
     }
   };
 
+  const handleBackgroundClick = (e) => {
+    if (e.target.id === "modal-background") closeModal();
+  };
+
   return (
-    <div className="delete-review-confirmation-modal">
-      <h2>Confirm Delete</h2>
-      <p>Are you sure you want to delete this review?</p>
-      {errors.length > 0 && (
-        <ul className="errors">
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
-      )}
-      <div className="modal-buttons">
-        <button onClick={handleDelete} className="confirm-delete-button">
-          Yes (Delete Review)
-        </button>
-        <button onClick={closeModal} className="cancel-button">
-          No (Keep Review)
-        </button>
+    <div id="modal-background" className="modal-background" onClick={handleBackgroundClick}>
+      <div className="delete-review-confirmation-modal">
+        <h2>Confirm Delete</h2>
+        <p>Are you sure you want to delete this review?</p>
+        {errors.length > 0 && (
+          <ul className="errors">
+            {errors.map((error, idx) => (
+              <li key={idx}>{error}</li>
+            ))}
+          </ul>
+        )}
+        <div className="modal-buttons">
+          <button onClick={handleDelete} className="confirm-delete-button">
+            Yes (Delete Review)
+          </button>
+          <button onClick={closeModal} className="cancel-button">
+            No (Keep Review)
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
 export default DeleteReviewConfirmationModal;
+
