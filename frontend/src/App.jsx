0e7de2d496a67cb1bef8b56efbbe722d422410f4
heather-yaple <heatherlyaple@gmail.com>
2024-11-25 16:58:19 -0500

    finally got the favicon to work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,69 @@
 // import React from 'react';
-import CounterComponent from './features/counter/CounterComponent';
-import LoginFormModal from './components/LoginFormPage/LoginFormModal';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { useModal } from './context/Modal';
 
-function App() {
+import Navigation from './components/Navigation/Navigation';
+import SearchBar from './components/Navigation/SearchBar';
+import ProfileButton from './components/Navigation/ProfileButton';
+import OpenModalMenuItem from './components/Navigation/OpenModalMenuItem';
+
+import LoginFormModal from './components/LoginFormPage/LoginFormModal';
+import SignupFormModal from './components/SignupFormPage/SignupFormModal';
+import SpotsIndex from './components/SpotsIndex/SpotsIndex';
+import SpotDetails from './components/SpotDetails/SpotDetails';
+import SpotFormModal from './components/SpotFormModal/SpotFormModal';
+import EditSpotFormModal from './components/EditSpotFormModal/EditSpotFormModal';
+import DeleteSpotConfirmationModal from './components/DeleteSpotConfirmationModal/DeleteSpotConfirmationModal';
+import ManageSpots from './components/ManageSpots/ManageSpots';
+import ReviewFormModal from './components/ReviewFormModal/ReviewFormModal';
+import EditReviewFormModal from './components/EditReviewFormModal/EditReviewFormModal';
+import DeleteReviewConfirmationModal from './components/DeleteReviewConfirmationModal/DeleteReviewConfirmationModal';
+
+import './App.css';
+import './index.css';
+
+const App = () => {
+  const { setModalContent, setOnModalClose } = useModal();
+
   return (
     <div>
-      <h1>Cozy Cabins</h1>
-      <LoginFormModal />
-      <CounterComponent />
+      <header>
+        <h1>Cozy Cabins</h1>
+        <Navigation isLoaded={true} />
+        <SearchBar />
+      </header>
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Navigate to="/spots" />} />
+          <Route path="/spots" element={<SpotsIndex />} />
+          <Route path="/spots/:spotId" element={<SpotDetails />} />
+          <Route path="/spots/new" element={<SpotFormModal />} />
+          <Route path="/spots/:spotId/edit" element={<EditSpotFormModal />} />
+          <Route path="/spots/:spotId/delete" element={<DeleteSpotConfirmationModal />} />
+          <Route path="/spots/:spotId/manage" element={<ManageSpots />} />
+          <Route path="/spots/:spotId/reviews/new" element={<ReviewFormModal />} />
+          <Route path="/spots/:spotId/reviews/:reviewId/edit" element={<EditReviewFormModal />} />
+          <Route path="/spots/:spotId/reviews/:reviewId/delete" element={<DeleteReviewConfirmationModal />} />
+        </Routes>
+      </main>
+
+      <footer>
+        <OpenModalMenuItem
+          itemText="Log In"
+          onItemClick={() => setModalContent(<LoginFormModal />)}
+          onModalClose={() => setOnModalClose()}
+        />
+        <OpenModalMenuItem
+          itemText="Sign Up"
+          onItemClick={() => setModalContent(<SignupFormModal />)}
+          onModalClose={() => setOnModalClose()}
+        />
+        <ProfileButton user={null} />
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
+
